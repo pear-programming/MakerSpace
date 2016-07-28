2cc31a4cbefa086c2ae6c57d84af966911a2bf5d
Jennifer <jmshakala@gmail.com>
2016-07-28 15:56:39 -0500

    Pw and email fields form validation

--- client/components/modal.js
+++ client/components/modal.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Popover, Button, Tooltip, Modal} from 'react-bootstrap';
+import {Popover, Button, Tooltip, Modal, FormGroup, FormControl, ControlLabel, HelpBlock} from 'react-bootstrap';
 
 export default class ModalButton extends Component {
   
@@ -8,7 +8,9 @@ export default class ModalButton extends Component {
 
     this.close = this.close.bind(this);
     this.open = this.open.bind(this);
+    this.validateEmail = this.validateEmail.bind(this);
     this.onInputChange = this.onInputChange.bind(this);
+    this.validatePassword = this.validatePassword.bind(this);
 
     this.state = { 
       showModal: false,
@@ -20,7 +22,14 @@ export default class ModalButton extends Component {
     var temp = this.state.user
     temp[inputField] = e.target.value
     this.setState({ user: Object.assign(this.state.user, temp) });
-    console.log('user object', this.state.user)
+    console.log('email', this.state.validateEmail)
+
+    if(inputField === 'email'){
+      this.setState({validEmail: this.validateEmail()});
+    }
+    if(inputField === 'password2' || inputField === 'password'){
+      this.setState({validPassword: this.validatePassword()});
+    }
   }
 
   onFormSubmit(e){
@@ -36,6 +45,23 @@ export default class ModalButton extends Component {
 
   open() {
     this.setState({ showModal: true });
+  }
+
+  validateEmail() {
+    var x = this.state.user.email;
+    var atpos = x.indexOf("@");
+    var dotpos = x.lastIndexOf(".");
+    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
+        return false;
+    }
+    return true;
+  }
+  validatePassword() {
+    if(this.state.user.password === this.state.user.password2) {
+      return true
+    } else {
+      return false
+    }
   }
 
   render() {
@@ -67,13 +93,27 @@ export default class ModalButton extends Component {
           <Modal.Body>
             <form>
               {this.props.mode === 'Sign Up' ? <span><label>Full Name</label><input className="form-control" onChange={this.onInputChange.bind(null, 'fullName')}/><br/></span> : null}
-              <span><label>Email</label><input className="form-control" onChange={this.onInputChange.bind(null, 'email')}/><br/></span>
+              
+              <FormGroup validationState={!this.state.validEmail ? "error" : "success" } onChange={this.onInputChange.bind(null, 'email')}>
+                <ControlLabel>Email</ControlLabel>
+                <FormControl type="text" />
+              </FormGroup>
+
               <span><label>Password</label><input type="password" className="form-control" onChange={this.onInputChange.bind(null, 'password')}/><br/></span>
-              {this.props.mode === 'Sign Up' ? <span><label>Re-enter Password</label><input type="password" className="form-control" onChange={this.onInputChange.bind(null, 'password2')}/><br/></span> : null}
+              {this.props.mode === 'Sign Up' ? 
+
+              <FormGroup validationState={!this.state.validPassword ? "error" : "success" } onChange={this.onInputChange.bind(null, 'password2')}>
+                <ControlLabel>Re-enter Password</ControlLabel>
+                <FormControl type="password" />
+                {!this.state.validPassword ? <HelpBlock>Passwords must match</HelpBlock> : null }
+              </FormGroup>
+              : null}
 
               <button type="submit">Submit</button>
 
             </form>
+
+
 
 
 
