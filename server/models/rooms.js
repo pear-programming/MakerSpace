99537696ae2a27f819a9fedde5ff40103bbff5b0
dilorc2 <dilorc2@gmail.com>
2016-07-30 12:19:01 -0500

    added get rooms functionality

--- server/models/rooms.js
+++ server/models/rooms.js
@@ -1,11 +1,11 @@
 var db = require('../db.js');
 
-var Room = module.exports 
+var Room = module.exports
 
 Room.addRooms = function(rooms, organizationId) {
 
 	// console.log("got room info after organization insert:", rooms, organizationId);
-	var roomsWithOrgId = rooms.map((room) => Object.assign(room, {organizationId: organizationId})); 
+	var roomsWithOrgId = rooms.map((room) => Object.assign(room, {organizationId: organizationId}));
 	console.log("after mapping:", roomsWithOrgId);
 	return db.rooms.insert(roomsWithOrgId)
 		.then(() => {
@@ -15,4 +15,6 @@ Room.addRooms = function(rooms, organizationId) {
 		.catch((err) => console.log("error inserting rooms:", err))
 }
 
-
+Room.findRooms = function(){
+	return db.collection('rooms').find({})
+}
