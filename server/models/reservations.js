4d931419520fcaa6a23c95fa79252d0224e0a271
dilorc2 <dilorc2@gmail.com>
2016-08-01 15:12:57 -0500

    changed tabs to spaces

--- server/models/reservations.js
+++ server/models/reservations.js
@@ -4,25 +4,22 @@ var Reservation = module.exports
 
 Reservation.findByRoomId = function(Id) {
 
-	console.log("inside reservations findbyid:", id);
-	return db.reservations.find({id: id})
-		.then((organizations) => {
-			return organizations
-
-		})
-
+  console.log("inside reservations findbyid:", id);
+  return db.reservations.find({id: id})
+    .then((organizations) => {
+    return organizations
+    })
 }
 
 Reservation.create = function(organizationData, userId) {
 
 
-	var newOrganization = Object.assign(organizationData, {users: []}, {adminIds:[userId]})
-	console.log("inside organization.create:", newOrganization);
+  var newOrganization = Object.assign(organizationData, {users: []}, {adminIds:[userId]})
+  console.log("inside organization.create:", newOrganization);
 
-	return db.organizations.insert(newOrganization)
-		.then((data) => {
-
-			console.log("successfully inserted organization!:", data)
-			return data;
-		})
+  return db.organizations.insert(newOrganization)
+    .then((data) => {
+      console.log("successfully inserted organization!:", data)
+      return data;
+    })
 }
