bb5dbc7d7c05fe570b46029c86ad178a6178e3e4
Burton Cooke <burtoncooke@yahoo.com>
2016-08-02 15:27:10 -0500

    reorganized reservation data

--- server/models/reservations.js
+++ server/models/reservations.js
@@ -21,7 +21,25 @@ Reservation.create = function(reservationData) {
 Reservation.findAllReservations = function() {
   return db.reservations.find({})
   .then(reservationsData => {
-    return reservationsData;
+
+    var roomReservations = reservationsData.reduce((accum, reservation) => {
+      var roomIds = accum.map((el) => el.roomId.toString())
+
+      var index = roomIds.indexOf(reservation.roomId.toString())
+      if(index !== -1) {
+        accum[index].reservations.push(reservation)
+        accum[index].reservations = accum[index].reservations.sort((a,b) => a.startTime - b.startTime);
+        return accum;
+      } 
+      else {
+        return accum.concat({roomName: reservation.roomName, roomId: reservation.roomId.toString(), reservations: [reservation]})
+      }
+    }, []).sort((a, b) => a.roomName.toLowerCase().charCodeAt(0) - b.roomName.toLowerCase().charCodeAt(0))
+
+      
+    console.log("showing test:", roomReservations);
+    // return reservationsData;
+    return roomReservations;
   })
 }
 
