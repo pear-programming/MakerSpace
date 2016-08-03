24d53e2a7d5598fdbbeadb081475ef3b5f4f266f
Burton Cooke <burtoncooke@yahoo.com>
2016-08-03 16:26:12 -0500

    instabook button working properly

--- client/components/rooms-list.js
+++ client/components/rooms-list.js
@@ -34,12 +34,37 @@ export default class RoomsList extends Component {
   updatedRooms(data) {
     this.setState({ rooms: data.rooms.rooms })
   }
+
+  instaBookedRoom(roomId) {
+    console.log("reached instaBookedRoom:", roomId);
+    console.log("showing rooms:", this.state.rooms);
+
+    var roomIndex; 
+    var bookedRoom = this.state.rooms.filter((room, index) => {
+
+      if(room._id.toString() === roomId.toString()) {
+        roomIndex = index;
+        return true;
+      }    
+      else {
+        return false
+      }
+    })[0] 
+
+    bookedRoom.isAvailable = false;
+    var roomsCopy = this.state.rooms.slice();
+    roomsCopy[roomIndex] = bookedRoom; 
+    console.log("showing rooms copy:", roomsCopy);
+    this.setState({rooms: roomsCopy})
+  }
+
   componentWillMount() {
     //ping server for latest room info then open socket to listen for someone else changing the state
     fetchRooms()
     .then( room => {
       console.log('room data', room)
-      socket.on('updatedRooms', this.updatedRooms.bind(this))
+      socket.on('updatedRooms', this.updatedRooms.bind(this));
+      socket.on('instaBooked', this.instaBookedRoom.bind(this));
       this.setState({ rooms: this.state.rooms.concat(room.data) })
     })
     .catch( err => {
