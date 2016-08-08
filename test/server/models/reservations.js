8bacaab100930a5ab80ddaf19a67fca043038aed
dilorc2 <dilorc2@gmail.com>
2016-08-08 15:15:48 -0500

    finished all testing for reservations endpoints

--- test/server/models/reservations.js
+++ test/server/models/reservations.js
@@ -40,44 +40,67 @@ describe('', function() {
         .expect(201)
         setTimeout(done, 15000);
     })
+    // it('Test 1 make a reservation', function(){
+    //   //  this.timeout(15000);
+    //   return client
+    //     .post('/reservations/new')
+    //     .send({
+    //     "_id": "57a4c24decafef3608011c52",
+    //     "new": "reservation"
+    //     })
+    //     .expect("57a4c24decafef3608011c")
+    //     .expect(201)
+    //     // setTimeout(done, 15000);
+    // })
     //
     it('Test 2  find by id', function(){
       return client
         .put('/reservations/57a4c24decafef3608011c52')
         .expect(200)
     })
+
+    it('Test 3  find by id', function(){
+      return client
+        .put('/reservations/57a4c24decafef3608011c')
+        .expect(500)
+    })
     //
-    it('Test 3 get all reservations', function(){
+    it('Test 4 get all reservations', function(){
       return client
         .get('/reservations')
         .expect(200)
     })
     //
-    it('Test 4 get reservations by name', function(){
+    it('Test 5 get reservations by name', function(){
       return client
         .get('/reservations/Dijkstra')
         .expect(200)
     })
+    it('Test 6 get reservations by name', function(){
+      return client
+        .get('/reservations/carlo')
+        .expect(400)
+    })
 
-    it('Test 5 delete a reservation', function(){//this works
+    it('Test 7 delete a reservation', function(){//this works
       return client
         .delete('/reservations/delete')
         .send({"new": "reservation"})
         .expect(201)
     })
-    it('Test 6 delete a reservation with {}', function(){//this works
+    it('Test 8 delete a reservation with {}', function(){//this works
       return client
         .delete('/reservations/delete')
         .send({})
         .expect(500)
     })
-    it('Test 7 delete a reservation with nothing', function(){//this works
+    it('Test 9 delete a reservation with nothing', function(){//this works
       return client
         .delete('/reservations/delete')
         .send()
         .expect(500)
     })
-    it('Test 8 delete a reservation with nothing', function(){//this works
+    it('Test 10 delete a reservation with nothing', function(){//this works
       return client
         .delete('/reservations/delete')
         .send('')
