e9637e7433022a1aa156435b249cf9260a754d46
dilorc2 <dilorc2@gmail.com>
2016-08-08 16:15:46 -0500

    final changes

--- server/models/reservations.js
+++ server/models/reservations.js
@@ -49,21 +49,21 @@ Reservation.findAllReservations = function() {
   return db.reservations.find({})//reservations
   .then(reservationsData => {
 
-    var roomReservations = reservationsData.reduce((accum, reservation) => {
-      var roomIds = accum.map((el) => el.roomId.toString())
-
-      var index = roomIds.indexOf(reservation.roomId.toString())
-      if(index !== -1) {
-        accum[index].reservations.push(reservation)
-        accum[index].reservations = accum[index].reservations.sort((a,b) => a.startTime - b.startTime);
-        return accum;
-      }
-      else {
-        return accum.concat({roomName: reservation.roomName, roomId: reservation.roomId, reservations: [reservation]})
-      }
-    }, []).sort((a, b) => a.roomName.toLowerCase().charCodeAt(0) - b.roomName.toLowerCase().charCodeAt(0))
-
-    return roomReservations;
+    // var roomReservations = reservationsData.reduce((accum, reservation) => {
+    //   var roomIds = accum.map((el) => el.roomId.toString())
+    //
+    //   var index = roomIds.indexOf(reservation.roomId.toString())
+    //   if(index !== -1) {
+    //     accum[index].reservations.push(reservation)
+    //     accum[index].reservations = accum[index].reservations.sort((a,b) => a.startTime - b.startTime);
+    //     return accum;
+    //   }
+    //   else {
+    //     return accum.concat({roomName: reservation.roomName, roomId: reservation.roomId, reservations: [reservation]})
+    //   }
+    // }, []).sort((a, b) => a.roomName.toLowerCase().charCodeAt(0) - b.roomName.toLowerCase().charCodeAt(0))
+    //
+    // return roomReservations;
 
     // makeSlots(reservationsData);
   //   db.users.find({})
