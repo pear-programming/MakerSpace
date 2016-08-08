e297a79646fe43e71a60e6f530648de715430b20
dilorc2 <dilorc2@gmail.com>
2016-08-08 15:51:22 -0500

    updated local file with master

--- server/models/reservations.js
+++ server/models/reservations.js
@@ -49,7 +49,6 @@ Reservation.findAllReservations = function() {
   return db.reservations.find({})//reservations
   .then(reservationsData => {
 
-<<<<<<< HEAD
     var roomReservations = reservationsData.reduce((accum, reservation) => {
       var roomIds = accum.map((el) => el.roomId.toString())
 
@@ -65,27 +64,26 @@ Reservation.findAllReservations = function() {
     }, []).sort((a, b) => a.roomName.toLowerCase().charCodeAt(0) - b.roomName.toLowerCase().charCodeAt(0))
 
     return roomReservations;
-=======
 
     // makeSlots(reservationsData);
   //   db.users.find({})
   //   .then((rows) => {
 
-  //     var userIds = rows.map((row) => row._id) 
-  //     var userNmes = rows.map((row) => row.name) 
+  //     var userIds = rows.map((row) => row._id)
+  //     var userNmes = rows.map((row) => row.name)
   //     console.log("got to users:", userNmes);
 
   //     db.rooms.find({})
   //       .then((data) => {
 
-  //         roomIds = data.map((dat) => dat._id) 
+  //         roomIds = data.map((dat) => dat._id)
   //         roomNms = data.map((dat) => dat.roomName)
   //         var usIds = userIds.slice(userIds.length - 10);
   //         var usNms = userNmes.slice(userNmes.length - 10);
   //         // console.log("userIds:", usIds)
   //         console.log("roomnames", roomNms)
 
-  //         // var usIds = userIds.slice(userIds.length - 10); 
+  //         // var usIds = userIds.slice(userIds.length - 10);
   //         var reservations = [
   //           {roomId: roomIds[0], roomName: roomNms[0], userId: usIds[5], userName: usNms[5], startTime: new Date(2016, 7, 5, 11, 0), endTime: new Date(2016, 7, 5, 11, 30)},
   //           {roomId: roomIds[2], roomName: roomNms[2], userId: usIds[5], userName: usNms[5], startTime: new Date(2016, 7, 6, 15, 0), endTime: new Date(2016, 7, 6, 16, 0)},
@@ -120,9 +118,9 @@ Reservation.findAllReservations = function() {
   //           .catch((err) => console.log("error inserting:", err))
   //       })
   // })
-    
+
     // console.log("after reservations.find")
-    
+
     // return reservationsData;
 
     // var roomReservations = reservationsData.reduce((accum, reservation) => {
@@ -133,7 +131,7 @@ Reservation.findAllReservations = function() {
     //     accum[index].reservations.push(reservation)
     //     accum[index].reservations = accum[index].reservations.sort((a,b) => a.startTime - b.startTime);
     //     return accum;
-    //   } 
+    //   }
     //   else {
     //     return accum.concat({roomName: reservation.roomName, roomId: reservation.roomId, reservations: [reservation]})
     //   }
@@ -146,7 +144,6 @@ Reservation.findAllReservations = function() {
     //   return timeSlots
     // })
     return reservationsData;
->>>>>>> d72f8cc5d9a773c8c9c77b4fe50ebb33de49c60c
   })
 }
 
@@ -190,9 +187,6 @@ Reservation.updateReservation = function(resId, newInfo) {
   })
   .catch(err => console.log('err in updateExisting: ', err))
 }
-<<<<<<< HEAD
-=======
-
 
 Reservation.makeSlots = function(reservationData) {
 
@@ -200,14 +194,14 @@ Reservation.makeSlots = function(reservationData) {
   var start = new Date(2016, 6, 31, 19, 0)
   var end = new Date(2016, 7, 31, 19, 0)
 
-  var thirtyMin = 1800000 
+  var thirtyMin = 1800000
   var startTime = Object.assign(start);
   var endTime = new Date(startTime.getTime() + thirtyMin);
 
   while(startTime.getTime() < end.getTime()) {
 
     timeSlots.push({startTime: startTime, endTime: endTime, reservations: [], isAvailable: true})
-    startTime = Object.assign(endTime); 
+    startTime = Object.assign(endTime);
     endTime = new Date(endTime.getTime() + thirtyMin)
   }
 
@@ -215,13 +209,13 @@ Reservation.makeSlots = function(reservationData) {
     .then((rooms) => {
 
       console.log("showing number of rooms:", rooms.length);
-      
+
     reservationData.forEach((reservation) => {
 
       timeSlots.forEach((timeSlot) => {
-       
-        if(reservation.startTime.getTime() == timeSlot.startTime.getTime() || 
-          (reservation.startTime.getTime() < timeSlot.startTime.getTime() && 
+
+        if(reservation.startTime.getTime() == timeSlot.startTime.getTime() ||
+          (reservation.startTime.getTime() < timeSlot.startTime.getTime() &&
             reservation.endTime.getTime() >= timeSlot.endTime.getTime())) {
           timeSlot.reservations.push(reservation)
         }
@@ -235,8 +229,3 @@ Reservation.makeSlots = function(reservationData) {
 
   })
 }
-
-
-
-
->>>>>>> d72f8cc5d9a773c8c9c77b4fe50ebb33de49c60c
