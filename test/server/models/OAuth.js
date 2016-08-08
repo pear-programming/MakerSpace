470732f80bb397a0cf9f76aefc9395181af3597c
dilorc2 <dilorc2@gmail.com>
2016-08-08 15:20:37 -0500

    Cleaned up OAuth testing endpoints

--- test/server/models/OAuth.js
+++ test/server/models/OAuth.js
@@ -5,10 +5,10 @@ import db from '../../../server/db';
 console.log(  "************************************\n"
             + "************************************\n"
             + "**                                **\n"
-            + "** Running Reservation's endpoint **\n"
-            + "**   HAVING ISSUES WITH LOGIN     **\n"
-            + "**    1) MakerPass Issus          **\n"
-            + "**    2) knex migrate:latest      **\n"
+            + "**     Running OAuth endpoint     **\n"
+            + "**                                **\n"
+            + "**                                **\n"
+            + "**                                **\n"
             + "**                                **\n"
             + "************************************\n"
             + "************************************\n" )
@@ -27,20 +27,6 @@ describe('', function() {
       client = request.agent(app)
     });
 
-
-    // it('Test 1 make a reservation', function(){
-    //    this.timeout(15000);
-    //   return client
-    //     .post('/reservations/new')
-    //     .send({
-    //     "_id": "57a4c24decafef3608011c52",
-    //     "new": "reservation"
-    //     })
-    //     .expect("57a4c24decafef3608011c52")
-    //     .expect(201)
-    //     setTimeout(done, 15000);
-    // })
-    //
     it('Test 1 direct browser to this route to start the OAuth dance', function(){
       return client
         .get('/auth/makerpass')
@@ -58,20 +44,6 @@ describe('', function() {
         .get('/app-bundle.js')
         .expect(200)
     })
-    // //
-    // it('Test 4 get reservations by name', function(){
-    //   return client
-    //     .get('/reservations/Dijkstra')
-    //     .expect(200)
-    // })
-    //
-    // it('Test 5 delete a reservation', function(){//this works
-    //   return client
-    //     .delete('/reservations/delete')
-    //     .send({"new": "reservation"})
-    //     .expect(201)
-    // })
-
 
   })
 
