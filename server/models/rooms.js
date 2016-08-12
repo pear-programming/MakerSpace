4f3202d5988e61b12afbecd494f29b9aaa7d6359
Caleb Anderson <caleb.anderson130@gmail.com>
2016-08-11 19:10:03 -0500

    collection to db

--- server/models/rooms.js
+++ server/models/rooms.js
@@ -7,7 +7,7 @@ Room.addRooms = function(rooms) {
   // console.log("got room info after organization insert:", rooms, organizationId);
   var roomsWithAvailability = rooms.map((room) => Object.assign(room, {isAvailable: true}));
   console.log("after mapping:", roomsWithAvailability);
-  return db.collection('rooms').insert(roomsWithAvailability)
+  return db.rooms.insert(roomsWithAvailability)
   .then(data => {
     return data.map((room) => room._id);
   })
@@ -16,28 +16,28 @@ Room.addRooms = function(rooms) {
 
 
 Room.deleteRoom = function(room) {
-  return db.collection('rooms').remove( { roomName: room } )
+  return db.rooms.remove( { roomName: room } )
 }
 
 
 Room.findRooms = function() {
   console.log("got into find rooms")
-  return db.collection('rooms').find({})
+  return db.rooms.find({})
 }
 
 
 Room.changeAvailability = function(roomName) {
-  return db.collection('rooms').find({roomName: roomName})
+  return db.rooms.find({roomName: roomName})
   .then(rooms => {
     if(rooms[0].isAvailable === true) {
       var updatedObj = rooms[0]
       updatedObj.isAvailable = false
-      db.collection('rooms').update({roomName: roomName}, updatedObj)
+      db.rooms.update({roomName: roomName}, updatedObj)
       return 'availability updated to false'
     } else {
       var updatedObj = rooms[0]
       updatedObj.isAvailable = true
-      db.collection('rooms').update({roomName: roomName}, updatedObj)
+      db.rooms.update({roomName: roomName}, updatedObj)
       return 'availability updated to true'
     }
   })
@@ -48,13 +48,13 @@ Room.updateRoom = function(roomId, newInfo) {
     roomId = db.ObjectId(roomId)
   }
 
-  return db.collection('rooms').update(
+  return db.rooms.update(
     {"_id" : roomId },
     { "$set" : newInfo }
   )
   .then(updatedRoom => {
     console.log('updatedRoom confermation: ', updatedRoom)
-    return db.collection('rooms').find({"_id":roomId})
+    return db.rooms.find({"_id":roomId})
   })
   .catch(err => console.log('err in updateExisting: ', err))
 }
