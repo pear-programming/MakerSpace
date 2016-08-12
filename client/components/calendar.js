5f77e80772cf3adc2ff3f0c67a6a3da450bd496b
Ashley Smith <ashleymarie4989@gmail.com>
2016-08-11 19:40:43 -0500

    remove sunday

--- client/components/calendar.js
+++ client/components/calendar.js
@@ -7,32 +7,6 @@ export default class Calendar extends React.Component {
     const { calendar } = this.refs;
 
     var open = this.props.open;
-
-    console.log('this.props.events as is', this.props.events)
-    // var events = this.props.events
-    // events.forEach(event => {
-    //   if(event.title === "Turing"){
-    //     event.color = "#5DB89D"
-    //   } else if (event.title === "Ellis"){
-    //     event.color = "#7FBFFF"
-    //   } else if (event.title === "Lovelace"){
-    //     event.color = "#0066CC"
-    //   } else if (event.title === "Hopper"){
-    //     event.color = "#BDD7EB"
-    //   } else if (event.title === "Dijkstra"){
-    //     event.color = "#186652"
-    //   } else if (event.title === "Tatooine"){
-    //     event.color = "#9FFF7F"
-    //   } else if (event.title === "Death Star"){
-    //     event.color = "#B266CC"
-    //   } else if (event.title === "Dagobah"){
-    //     event.color = "#DFFF7F"
-    //   } else if (event.title === "Naboo"){
-    //     event.color = "#B296C7"
-    //   } else if (event.title === "Lecture hall"){
-    //     event.color = "#66CCCC"
-    //   }
-    // })
   
     $(calendar).fullCalendar({
       events: this.props.events,
@@ -44,6 +18,7 @@ export default class Calendar extends React.Component {
         open(new Date(2016, date._d.getMonth(), date._d.getDate(), 4, 0));
       },
       allDay: false,
+      hiddenDays: [ 0 ],
       minTime: "09:00:00",
       maxTime: "21:00:00",
       height: "auto"  
